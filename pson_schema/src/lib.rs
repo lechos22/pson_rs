5001caf4bb595156379120b32807ce2528d6d349
lechos22 <mazurlech8@gmail.com>
2023-02-09 20:01:45 +0100

    reformatted code

--- pson_schema/src/lib.rs
+++ pson_schema/src/lib.rs
@@ -1,12 +1,15 @@
-use std::{collections::{HashMap, hash_map::DefaultHasher}, hash::{Hasher, Hash}};
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap},
+    hash::{Hash, Hasher},
+};
 
-use proc_macro::{TokenStream, TokenTree, Delimiter};
+use proc_macro::{Delimiter, TokenStream, TokenTree};
 
-struct Pairs<T>{
-    iter: Box<dyn Iterator<Item = T>>
+struct Pairs<T> {
+    iter: Box<dyn Iterator<Item = T>>,
 }
 
-impl Iterator<> for Pairs<TokenTree>{
+impl Iterator for Pairs<TokenTree> {
     type Item = (TokenTree, TokenTree);
     fn next(&mut self) -> Option<Self::Item> {
         match (self.iter.next(), self.iter.next()) {
@@ -17,18 +20,18 @@ impl Iterator<> for Pairs<TokenTree>{
     }
 }
 
-struct PsonDef{
+struct PsonDef {
     name: String,
     body: Option<String>,
     children: Option<Vec<PsonDef>>,
 }
 
-impl PsonDef{
+impl PsonDef {
     fn primitive(name: String) -> PsonDef {
-        PsonDef{
+        PsonDef {
             name,
             body: None,
-            children: None
+            children: None,
         }
     }
     fn to_flat_iter(&self) -> Vec<String> {
@@ -46,15 +49,15 @@ impl PsonDef{
 }
 
 fn pson_primitive(name: &str) -> PsonDef {
-    PsonDef::primitive(match name{
+    PsonDef::primitive(match name {
         "string" => "String".to_string(),
         "unsgn" => "u64".to_string(),
         "int" => "i64".to_string(),
         "float" => "f64".to_string(),
         "bool" => "bool".to_string(),
         "null" => "()".to_string(),
-        tag if(tag.starts_with("_")) => name[1..].to_string(),
-        _ => panic!("Unrecognized primitive type: {}", name)
+        tag if (tag.starts_with("_")) => name[1..].to_string(),
+        _ => panic!("Unrecognized primitive type: {}", name),
     })
 }
 
@@ -64,7 +67,7 @@ fn pson_struct_tuple(token_trees: Vec<TokenTree>) -> PsonDef {
     }
     let kind = match &token_trees[0] {
         TokenTree::Ident(kind) => kind.to_string(),
-        _ => panic!("Struct type must be an identifier")
+        _ => panic!("Struct type must be an identifier"),
     };
     let kind_str = kind.as_str();
     fn single_typed(kind: &str, token_tree: TokenTree) -> PsonDef {
@@ -73,10 +76,10 @@ fn pson_struct_tuple(token_trees: Vec<TokenTree>) -> PsonDef {
         kind.hash(&mut hasher);
         body.name.hash(&mut hasher);
         let name = format!("Pson{}", hasher.finish());
-        PsonDef{
+        PsonDef {
             name: name.clone(),
             body: Some(format!("type {}=Vec<{}>;", name.clone(), body.name)),
-            children: Some(vec![body])
+            children: Some(vec![body]),
         }
     }
     fn map(kind: &str, pairs: Pairs<TokenTree>) -> PsonDef {
@@ -85,7 +88,7 @@ fn pson_struct_tuple(token_trees: Vec<TokenTree>) -> PsonDef {
         kind.hash(&mut hasher);
         let mut children = Vec::<PsonDef>::new();
         let body_inner = pairs
-            .map(|(key, value)|{
+            .map(|(key, value)| {
                 key.to_string().hash(&mut hasher);
                 let value = parse_pson_schema(value);
                 value.name.hash(&mut hasher);
@@ -94,15 +97,17 @@ fn pson_struct_tuple(token_trees: Vec<TokenTree>) -> PsonDef {
             })
             .fold(
                 String::new(),
-                |body_builder, (child, code)|
-                {children.push(child); body_builder + &code + ","} // this is still bad, I should find another way to do this
+                |body_builder, (child, code)| {
+                    children.push(child);
+                    body_builder + &code + ","
+                }, // this is still bad, I should find another way to do this
             );
         let name = format!("Pson{}", hasher.finish());
         let body = format!("struct {}{{{}}}", name.clone(), body_inner);
-        PsonDef{
+        PsonDef {
             name: name.clone(),
             body: Some(body),
-            children: Some(children)
+            children: Some(children),
         }
     }
     match kind_str {
@@ -112,30 +117,27 @@ fn pson_struct_tuple(token_trees: Vec<TokenTree>) -> PsonDef {
                     if group.delimiter() != Delimiter::Parenthesis {
                         panic!("Map body must be a parenthesized list of key-value pairs")
                     }
-                    Pairs{iter: Box::from(group.stream().into_iter())}
-                },
-                _ => panic!("Map body must be a parenthesized list of key-value pairs")
+                    Pairs {
+                        iter: Box::from(group.stream().into_iter()),
+                    }
+                }
+                _ => panic!("Map body must be a parenthesized list of key-value pairs"),
             };
             map(kind_str, pairs)
-        },
+        }
         "array" | "option" => single_typed(kind_str, token_trees[1].clone()),
         "tuple" => todo!(),
-        _ => panic!("Struct type must be one of map, array, option or tuple")
+        _ => panic!("Struct type must be one of map, array, option or tuple"),
     }
 }
 
 fn parse_pson_schema(input: TokenTree) -> PsonDef {
     match input {
-        TokenTree::Group(pson_object) => {
-            match pson_object.delimiter() {
-                Delimiter::Bracket => pson_struct_tuple(
-                    pson_object
-                        .stream()
-                        .into_iter()
-                        .collect::<Vec<_>>()
-                ),
-                _ => panic!("Unrecognized delimiter: {:?}", pson_object.delimiter())
+        TokenTree::Group(pson_object) => match pson_object.delimiter() {
+            Delimiter::Bracket => {
+                pson_struct_tuple(pson_object.stream().into_iter().collect::<Vec<_>>())
             }
+            _ => panic!("Unrecognized delimiter: {:?}", pson_object.delimiter()),
         },
         TokenTree::Ident(name) => pson_primitive(name.to_string().as_str()),
         _ => panic!("Unrecognized token: {}", input),
@@ -144,33 +146,28 @@ fn parse_pson_schema(input: TokenTree) -> PsonDef {
 
 #[proc_macro]
 pub fn pson_schemas(input: TokenStream) -> TokenStream {
-    let iter = Pairs{iter: Box::new(input.into_iter())};
+    let iter = Pairs {
+        iter: Box::new(input.into_iter()),
+    };
 
     let schema: HashMap<String, PsonDef> = iter
-        .map(|(key, value)|{
-            match key {
-                TokenTree::Ident(name) => (name.to_string(), value),
-                _ => panic!("Key must be an identifier")
-            }
+        .map(|(key, value)| match key {
+            TokenTree::Ident(name) => (name.to_string(), value),
+            _ => panic!("Key must be an identifier"),
         })
-        .map(|(name, value)|{
-            (name, parse_pson_schema(value))
-        })
+        .map(|(name, value)| (name, parse_pson_schema(value)))
         .collect();
 
     let iter_base = schema
         .iter()
-        .map(|(name, object)|{
-            format!("type {}={};", name, object.name)
-        });
-    let iter_bodies = schema
-        .iter()
-        .flat_map(|(_, object)|{
-            object.to_flat_iter()
-        });
+        .map(|(name, object)| format!("type {}={};", name, object.name));
+    let iter_bodies = schema.iter().flat_map(|(_, object)| object.to_flat_iter());
     iter_base
         .chain(iter_bodies)
-        .map(|s| {println!("{}", s); s}) // TODO: Remove this
+        .map(|s| {
+            println!("{}", s);
+            s
+        }) // TODO: Remove this
         .collect::<String>()
         .parse()
         .unwrap()
