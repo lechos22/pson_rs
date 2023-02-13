0f251813eff911e890d872e8cf28599b2a3eb42a
lechos22 <mazurlech8@gmail.com>
2023-02-13 15:00:10 +0100

    started working on module compiling

---
+++ pson_lambda/src/compiler.rs
@@ -0,0 +1,34 @@
+use std::{error::Error, collections::HashSet};
+
+use pson::{PsonParser, Expr};
+
+fn scan_for_constants(ast: Vec<Expr>) -> HashSet<String>{
+    ast
+        .iter()
+        .filter_map(|expr| expr.as_array())
+        .filter(|arr| arr.len() == 3)
+        .filter_map(|arr| {
+            match &arr[0].as_string().map(|n| n == "$"){
+                Some(true) => Some(arr),
+                _ => None,
+            }
+        })
+        .filter_map(|arr| {
+            match &arr[1].as_string(){
+                Some(name) => Some(name.clone()),
+                None => None,
+            }
+        })
+        .collect()
+}
+
+pub(crate) fn compile_module(code: &str) -> Result<(), Box<dyn Error>> {
+    let mut parser = PsonParser::new(code.chars());
+    parser.parse()?;
+    let ast = parser.get()?;
+    println!("{}", ast.to_string());
+    // search for global constants(functions included) defined as [$ name value]
+    let constants = scan_for_constants((&ast).as_array().unwrap());
+    println!("module constants: {:?}", constants);
+    Ok(())
+}
--- pson_lambda/src/main.rs
+++ pson_lambda/src/main.rs
@@ -1,9 +1,14 @@
 use std::env::args;
+use std::fs::File;
+use std::io::Read;
 
 mod arg_parser;
 mod config;
 mod repl;
 mod context;
+mod compiler;
+#[cfg(test)]
+mod tests;
 
 fn main() {
     let arg_config = arg_parser::parse_args(&mut args().skip(1)).unwrap();
@@ -13,6 +18,13 @@ fn main() {
         repl::repl();
     }
     else {
-        println!("Running file: {}", arg_config.file.unwrap());
+        let file_name = arg_config.file.unwrap();
+        let file = File::open(file_name).unwrap();
+        let content = file
+            .bytes()
+            .map(|b| b.unwrap())
+            .collect::<Vec<u8>>();
+        let content_str = String::from_utf8_lossy(content.as_slice());
+        compiler::compile_module(content_str.as_ref()).unwrap();
     }
 }
