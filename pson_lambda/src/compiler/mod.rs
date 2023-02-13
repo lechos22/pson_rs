7a8dc34f0636193ec125ecfbbad4ca17394bf30b
lechos22 <mazurlech8@gmail.com>
2023-02-13 18:30:35 +0100

    renamed a variable to be more accurate

--- pson_lambda/src/compiler/mod.rs
+++ pson_lambda/src/compiler/mod.rs
@@ -5,7 +5,7 @@ use pson::{PsonParser, Expr};
 pub(crate) fn compile_module(code: &str) -> Result<(), Box<dyn Error>> {
     let mut parser = PsonParser::new(code.chars());
     parser.parse()?;
-    let ast = parser.get()?;
-    println!("{}", ast.to_string());
+    let code_tree = parser.get()?;
+    println!("{}", code_tree.to_string());
     Ok(())
 }
